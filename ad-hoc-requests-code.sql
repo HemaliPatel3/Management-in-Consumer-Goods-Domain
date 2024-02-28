b4bea5c4c735c1372eb7502176f20acecf73b7e0
Hemali Patel <81223796+HemaliPatel3@users.noreply.github.com>
2024-02-28 05:42:11 +0530

    Update ad-hoc-requests-code.sql

--- ad-hoc-requests-code.sql
+++ ad-hoc-requests-code.sql
@@ -10,6 +10,14 @@ final output contains these fields,
 unique_products_2020 
 unique_products_2021 
 percentage_chg */
+SELECT X.A AS unique_product_2020, Y.B AS unique_products_2021, ROUND((B-A)*100/A, 2) AS percentage_chg
+FROM
+     (
+      (SELECT COUNT(DISTINCT(product_code)) AS A FROM fact_sales_monthly
+      WHERE fiscal_year = 2020) X,
+      (SELECT COUNT(DISTINCT(product_code)) AS B FROM fact_sales_monthly
+      WHERE fiscal_year = 2021) Y 
+	 )
 
 
 
