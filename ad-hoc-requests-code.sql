8d51dc8c7b0ec86dd01683a4c6a5211a45bb2721
Hemali Patel <81223796+HemaliPatel3@users.noreply.github.com>
2024-03-02 11:33:34 +0530

    Update ad-hoc-requests-code.sql

--- ad-hoc-requests-code.sql
+++ ad-hoc-requests-code.sql
@@ -63,6 +63,14 @@ The final output should contain these fields,
 product_code 
 product 
 manufacturing_cost */
+select p.product_code , p.product , m.manufacturing_cost
+from gdb023.dim_product p
+join fact_manufacturing_cost as m
+using(product_code)
+where 
+	manufacturing_cost=(select max(manufacturing_cost) from fact_manufacturing_cost) or 
+	manufacturing_cost=(select min(manufacturing_cost) from fact_manufacturing_cost)
+order by manufacturing_cost desc;
 
 
 
